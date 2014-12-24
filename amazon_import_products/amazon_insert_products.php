b3ab11f2d41cb53af158040ab2a43defdd4d080c
VastEdge-GitHub <upinder@essitco.com>
2014-12-24 19:14:19 +0530

    Monthly Price issue
    Monthly Price issue

--- amazon_import_products/amazon_insert_products.php
+++ amazon_import_products/amazon_insert_products.php
@@ -724,10 +724,11 @@ session_start();
 											}
 											//// Standard Shipping Calculation Ends \\\\
 											$prod_price 		= $prod_price+$standard_shipping;							// Final Price = MSRP+Shipping Cost	
+											$RD_price			= ceil($prod_price)-0.01;
 											if($prod_price <= '2500')
 											 {
-												$prod_tax = ($prod_price * 8.3)/100;						//Calculating tax price over amazon price
-												$price_with_tax_shipping = $prod_tax+$prod_price;							//Price included Tax and shipping
+												$prod_tax = ($RD_price * 8.3)/100;											//Calculating tax price over amazon price
+												$price_with_tax_shipping = $prod_tax+$RD_price;							//Price included Tax and shipping
 												$liki_disc			= '15';														// 15%
 												$liki_price			= ($liki_disc/100)*$price_with_tax_shipping;				// LIKI price calculation
 												$liki_price			= number_format($liki_price, 2, '.', ',');					// Value upto 2 decimals
@@ -794,12 +795,12 @@ session_start();
 												$replace_spcl_chars	= array(" ",","," "," "," "," ");
 												
 												$mainfile_fh		= fopen($base_url_magento.'var/import/main_file_import_new.csv', 'a');								
-												$_category = Mage::getModel('catalog/category')->load($prod_categoryid);		//Magento Product Category name
+												$_category = Mage::getModel('catalog/category')->load($prod_categoryid);		//Magento Product Category name 
 												$category_Name = $_category->getName();
 												$parentCategoryId = $_category->getParentId();
 											
 											fwrite($mainfile_fh,'^admin^|^base^|^Default^|^simple^|^'.$prod_categoryid.'^|^'.$prod_sku.'^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,utf8_encode(ltrim(rtrim($prod_name,'"'),'"'))).'^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,ltrim(rtrim($new_main_img,'"'),'"')).'^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,ltrim(rtrim($new_smail_img,'"'),'"')).'^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,ltrim(rtrim($new_smail_img,'"'),'"')).'^|^'.$prod_amazonprime.'^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,ltrim(rtrim($prod_likidesc,'"'),'"')).'^|^'.$meta_tag_robot.'^|^'.$prod_price.'^|^'.$prod_weight.'^|^'.$standard_shipping.'^|^'.$prod_status.'^|^'.$prod_visibility.'^|^'.$prod_tax_class.'^|^'.$amazon_sync.'^|^'.$amazon_use_categories.'^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,utf8_encode(ltrim(rtrim($prod_desc,'"'),'"'))).'^|^'.utf8_encode($prod_shortdesc).'^|^'.$amazon_local.'^|^'.$amazon_asin.'^|^'.$amazon_ean.'^|^'.$amazon_offer_condition.'^|^'.$amazon_offer_price_type.'^|^'.$amazon_offer_price.'^|^'.$amazon_offer_currency.'^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,ltrim(rtrim($amazon_offers_list_url,'"'),'"')).'^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,ltrim(rtrim($amazon_product_url,'"'),'"')).'^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,ltrim(rtrim($amazon_reviews_url,'"'),'"')).'^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,ltrim(rtrim($liki_price,'"'),'"')).'^|^'.$prod_qty.'^|^0^|^1^|^0^|^0^|^0^|^1^|^10000^|^1^|^'.$prod_instock.'^|^1^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,utf8_encode(ltrim(rtrim($prod_name,'"'),'"'))).'^|^0^|^simple^|^'.$prod_salesrank.'^|^'.$prod_brand.
-					'^|^'.'Buy rent-to-own'.str_replace($search_spcl_chars,$replace_spcl_chars,utf8_encode(ltrim(rtrim($prod_name,'"'),'"'))).'now and pay later with affordable monthly payments. We stock up with more than 10,000 products from leading brands^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,utf8_encode(ltrim(rtrim($prod_name,'"'),'"'))).',shop '.strtolower($category_Name).' on monthly installments, LIKI, rent to own, lease to own, buy now pay later, low monthly payments^'.PHP_EOL);
+					'^|^'.'Buy rent-to-own '.str_replace($search_spcl_chars,$replace_spcl_chars,utf8_encode(ltrim(rtrim($prod_name,'"'),'"'))).' now and pay later with affordable monthly payments. We stock up with more than 10,000 products from leading brands^|^'.str_replace($search_spcl_chars,$replace_spcl_chars,utf8_encode(ltrim(rtrim($prod_name,'"'),'"'))).',shop '.strtolower($category_Name).' on monthly installments, LIKI, rent to own, lease to own, buy now pay later, low monthly payments^'.PHP_EOL);
 												
 												fclose($mainfile_fh);
 											}	// End of Stopword list check
