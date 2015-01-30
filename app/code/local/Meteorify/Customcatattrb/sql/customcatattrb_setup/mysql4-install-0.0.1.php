6ef1522f9a2203d53f49f43b2a1ce164fdc54ffd
VastEdge-GitHub <upinder@essitco.com>
2015-01-30 12:33:42 +0530

    Write and Add Basic Category Page Text
    Write and Add Basic Category Page Text

---
+++ app/code/local/Meteorify/Customcatattrb/sql/customcatattrb_setup/mysql4-install-0.0.1.php
@@ -0,0 +1,18 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+$attribute  = array(
+    'type' => 'text',
+    'label'=> 'Bottom Description',
+    'input' => 'textarea',
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'visible' => true,
+    'required' => false,
+    'user_defined' => true,
+    'default' => "",
+	'sort_order' => 9,
+    'group' => "General Information" 
+   );
+$installer->addAttribute('catalog_category', 'bottom_description', $attribute);
+$installer->endSetup();
+?>
