086786d30d07de4deba52f38d318e08c1e96982e
VastEdge-GitHub <upinder@essitco.com>
2015-02-13 18:48:00 +0530

    New Queries Sync scripts
    New Queries Sync scripts

--- amazon_import_products/qmt_mysqldump/Updated_query_pull.php
+++ amazon_import_products/qmt_mysqldump/Updated_query_pull.php
@@ -1,11 +1,16 @@
 <?php
-	$last_line = system('wget http://demo.retaildeal.biz/amazon_import_products/qmt_mysqldump/query.sql',$retval);
-	if($retval==0)
-	{
-		$last_line = system('mysql -ubn_magento -pfbeee979d3 rd_qmt < /opt/bitnami/apps/magento/htdocs/amazon_import_products/qmt_mysqldump/query.sql',$retval);
+		$last_line = system('wget http://demo.retaildeal.biz/amazon_import_products/qmt_mysqldump/query.sql',$retval);
 		if($retval==0)
 		{
-			system('rm -rf /opt/bitnami/apps/magento/htdocs/amazon_import_products/qmt_mysqldump/query.sql',$retval);
+			$input='pwd';
+			$output = shell_exec($input);
+			echo "<pre>$output</pre>";
+			$target = trim($output).'/query.sql';
+			$input = 'mysql -ubn_magento -pfbeee979d3 rd_qmt < '.$target;
+			echo $input;
+			$output = shell_exec($input);
+			$input = 'rm -rf '.$target;
+			echo $input;
+			if($output==''){ shell_exec($input);}
 		}
-	}
 ?>
